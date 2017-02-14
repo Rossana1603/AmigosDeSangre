ca30a7e6a15eee86eac198a8f87219efacbfbe6b
Rossana-PC\Rossana <rossana1603@gmail.com>
2017-02-14 06:44:35 -0500

    add model

---
+++ AmigosDeSangre.Datos/Modelos/Horario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmigosDeSangre.Datos.Modelos
+{
+    public class Horario
+    {
+        public int CodigoHorario { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual DetalleHorario DetalleHorario { get; set; }
+    }
+}
